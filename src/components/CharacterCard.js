fbd9b27a57a98d13dbddb73e8ae95e9a3406ec0d
Zak McRae <zakmcrae@gmail.com>
2021-08-23 12:12:03 -0400

    added ids to elements
    id from event used in click character card function

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,8 +3,12 @@ import "./CharacterCard.css";
 function CharacterCard(props) {
   return (
     <div className="character-card" id={props.characterData.char_id}>
-      <img src={props.characterData.img} alt={props.characterData.name}></img>
-      <p>{props.characterData.name}</p>
+      <img
+        src={props.characterData.img}
+        alt={props.characterData.name}
+        id={props.characterData.char_id}
+      ></img>
+      <p id={props.characterData.char_id}>{props.characterData.name}</p>
     </div>
   );
 }
--- src/components/CharacterGrid.js
+++ src/components/CharacterGrid.js
@@ -3,7 +3,7 @@ import "./CharacterGrid.css";
 
 function CharacterGrid(props) {
   return (
-    <div className="character-grid">
+    <div className="character-grid" onClick={props.clickCharacterCard}>
       {props.characters.slice(0, props.limit).map((char) => {
         return <CharacterCard key={char.char_id} characterData={char} />;
       })}
